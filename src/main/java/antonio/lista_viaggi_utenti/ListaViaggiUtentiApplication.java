139612dfd4488cc79819615eaf98b86a3ee467b7
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-11-08 10:38:05 +0100

    Creazione DB , Creazione di soli Package , Configurazione Env Properties e App. Properties  , Modifica Pom.xml

---
+++ src/main/java/antonio/lista_viaggi_utenti/ListaViaggiUtentiApplication.java
@@ -0,0 +1,13 @@
+package antonio.lista_viaggi_utenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaViaggiUtentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaViaggiUtentiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antonio/lista_viaggi_utenti/ListaViaggiUtentiApplicationTests.java
@@ -0,0 +1,13 @@
+package antonio.lista_viaggi_utenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaViaggiUtentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
